8520127dfcd7d120a4177b8100475f51316f1bbe
caiooliveirah09 <leelros@hotmail.com>
2023-03-03 22:42:11 -0300

    solved requirement 6

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -59,4 +59,8 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    job_filtered_by_type = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            job_filtered_by_type.append(job)
+    return job_filtered_by_type
