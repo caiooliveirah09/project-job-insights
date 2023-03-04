cec80da007c776f5450bd77866955ec1f4c3631b
caiooliveirah09 <leelros@hotmail.com>
2023-03-03 22:25:19 -0300

    i solved requirement 3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +17,12 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    data = read(path)
+    industries = []
+    for job in data:
+        if job["industry"] and job["industry"] not in industries:
+            industries.append(job["industry"])
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
